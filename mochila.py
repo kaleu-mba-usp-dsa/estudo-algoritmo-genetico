31dbe1f8a373f78a049dc5f14d2f345da8665283
Kaléu Puskas <kaleu@konscia.me>
2025-08-27 18:09:44 -0300

    Rodando gerações com inclusão de mutação de genes

--- mochila.py
+++ mochila.py
@@ -10,6 +10,7 @@ print('PROBLEMA MOCHILA')
 # Gerar indivíduos o bastante até completar a geração.
 
 param_numero_individuos = 10
+param_percentual_chance_mutacao = 0.1
 populacao = []
 
 while len(populacao) < param_numero_individuos:
@@ -20,7 +21,7 @@ while len(populacao) < param_numero_individuos:
 print(f'Gerou população de {len(populacao)} indivíduos')
 
 # ### AVALIA MELHOR SOLUÇÃO ###
-
+geracoes = []
 fitness = 0
 best_solution = None
 
@@ -28,46 +29,56 @@ def avalia_fitness(_populacao):
     global fitness
     global best_solution
 
+    _valores = []
     for i in _populacao:
         valor = pega_total_valor(i)
+        _valores.append(valor)
         if valor > fitness:
             fitness = valor
             best_solution = i
 
-    print(f'Fitness: {fitness}')
-    exibe_dados(best_solution)
+    print(f'Fitness +alto: {fitness}')
+    print(f'Média Valores: {sum(_valores) / len(_valores):2f}')
     print('')
 
 avalia_fitness(populacao)
 
-# ### SELECIONA PAIS PARA UMA NOVA RODADA ###
-### Vou usar uma solução simples, pegando os 60% melhores
-### E realizando cruzamentos aleatórios entre eles para gerar os novos 8
-### Manterei os dois melhores da geração anterior como um processo de elitismo
+max_geracoes = 10
+for i in range(max_geracoes):
+    # ### SELECIONA PAIS PARA UMA NOVA RODADA ###
+    ### Vou usar uma solução simples, pegando os 60% melhores
+    ### E realizando cruzamentos aleatórios entre eles para gerar os novos 8
+    ### Manterei os dois melhores da geração anterior como um processo de elitismo
 
-populacao_ordenada = sorted(populacao, key=lambda item: pega_total_valor(item), reverse=True)
-nova_populacao = []
-nova_populacao.append(populacao_ordenada[0].copy())
-nova_populacao.append(populacao_ordenada[1].copy())
+    populacao_ordenada = sorted(populacao, key=lambda item: pega_total_valor(item), reverse=True)
+    nova_populacao = []
+    # nova_populacao.append(populacao_ordenada[0].copy())
+    # nova_populacao.append(populacao_ordenada[1].copy())
 
-repeticoes = 0
-while len(nova_populacao) < 10 and repeticoes < 100:
-    repeticoes += 1
+    repeticoes = 0
+    while len(nova_populacao) < 10 and repeticoes < 100:
+        repeticoes += 1
 
-    a, b = sorteia_dois_diferentes(0, 5)
-    pai = populacao_ordenada[a]
-    mae = populacao_ordenada[b]
+        a, b = sorteia_dois_diferentes(0, 5)
+        pai = populacao_ordenada[a]
+        mae = populacao_ordenada[b]
 
-    # pega primeira parte do pai e segunda parte da mae
-    filho = pai[:4] + [random.choice([pai[4], mae[4]])] + mae[5:]
-    peso = pega_total_peso(filho)
+        # pega primeira parte do pai e segunda parte da mae
+        filho = pai[:4] + [random.choice([pai[4], mae[4]])] + mae[5:]
 
-    if peso <= 5000:
-        nova_populacao.append(filho)
+        # Chance de mutação de 10%
+        for index, gene in enumerate(filho):
+            if random.random() < param_percentual_chance_mutacao:
+                filho[index] = 1 - gene
 
-print(f'Gerou nova população de {len(nova_populacao)} indivíduos')
-avalia_fitness(nova_populacao)
+        peso = pega_total_peso(filho)
 
+        if peso <= 5000:
+            nova_populacao.append(filho)
 
+    print(f'Geração {i + 1}')
+    print(f'Gerou nova população de {len(nova_populacao)} indivíduos')
+    avalia_fitness(nova_populacao)
+    populacao = nova_populacao
 
-
+exibe_dados(best_solution)
